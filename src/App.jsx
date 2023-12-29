8fce017fd00e456d42ca514e0166400dbee243f5
ramees <aleemrameestpn@gmail.com>
2023-12-29 09:24:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
+
+function App() {
+   return (
+      <div className="app">
+         <Header />
+         <Navigation />
+         <Main />
+         <Footer />
+      </div>
+   );
+}
+
+export default App;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navigation() {
+   return (
+      <div className="navigation-bar">
+         <h1>Navigation Bar</h1>
+      </div>
+   );
+}
+
+export default Navigation;
